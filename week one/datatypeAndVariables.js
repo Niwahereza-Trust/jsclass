db15ae79ab7d8b94e24d997e829d453ad31b1400
trust <niwaherezatrust6@gmail.com>
2025-07-30 08:27:35 +0300

    datatypeAndVariables.js

---
+++ week one/datatypeAndVariables.js
@@ -0,0 +1,59 @@
+/*
+  1.strings/
+  2.numbers
+  intergers
+  big intergers
+  decimals
+  3. booleans
+  true or false
+  4.underfined
+  5.nul 
+  let x-0
+  6 symbols
+  uniqness*/
+
+
+
+
+  productname="timber"//sting we put them in quotes
+let productcost =3500
+let transportisneed=true 
+let tablecolour
+let discount=null
+console.log(productcost*2) 
+/*non primitive data type
+1. array
+     is  a list off item
+     [numbers]
+     [2,4,2,0]
+     ["sofa, table,cupboard"]
+2.   list productlists =["sofas",tables","cupboards"]
+    object
+    in js everything is an object 
+    {
+    name:sofa
+    price:3000
+    color:blue
+    }
+    data
+    function*/
+
+
+    let productlist=["sofas",tables,"cupboards"]
+    console.log(productlist)
+
+
+    let dinningTableobject={
+        productName: "dining table",
+        quanaty:2,
+        price:350,
+        totalcost:quantity * price
+    }
+    let  productName ="dining table"
+    let  quanaty =2
+    let    price =350
+    let   totalcost =quantity * price
+    console.log=(totalcost)
+
+    let paymenttype=cost 
+    cosnt= VAT
---
+++ week one/index.js
@@ -0,0 +1,12 @@
+console.log("hello, world");
+
+let b=2
+let a=3
+let : b+a;
+consolo.log (sum)
+
+let c=5
+let y=2
+consolo.log(multiplication)
+//comment
+/*muiltple comment*/
